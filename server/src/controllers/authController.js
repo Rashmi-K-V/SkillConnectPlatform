1c3e89dd8910743768d85183d73f755a4ddf88ca
Rashmi <rashmikvaidya11@gmail.com>
2026-03-07 00:04:09 +0530

    Portfolio Schema Designed

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -45,7 +45,7 @@ const login = async(req,res) =>{
         id:user._id,
         role:user.role,
       },
-      proccess.env.JWT_SECRET,
+      process.env.JWT_SECRET,
       {expiresIn:"7d"}
     );
     res.json({token,user});
