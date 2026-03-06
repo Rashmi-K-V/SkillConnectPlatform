baa54390bbceb57a8de83dbae7dd26f947de5447
Rashmi <rashmikvaidya11@gmail.com>
2026-03-06 12:33:41 +0530

    Login and routes for auth added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -28,3 +28,30 @@ const register = async(req , res) => {
     res.status(500).json({message:error.message});
   }
 }
+
+const login = async(req,res) =>{
+  try{
+    const { email, password } = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({message:"Invalid credentials"});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return res.status(400).json({message:"Invalid credentials"});
+    }
+    const token = jwt.sign(
+      {
+        id:user._id,
+        role:user.role,
+      },
+      proccess.env.JWT_SECRET,
+      {expiresIn:"7d"}
+    );
+    res.json({token,user});
+  }catch(error){
+    res.status(500).json({message:error.message});
+  }
+}
+
+module.exports = { register, login };
